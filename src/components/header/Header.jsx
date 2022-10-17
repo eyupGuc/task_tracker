307ba1e391810971941c555addb3bcf1adf17dba
eyupGuc <meydanguresi@gmail.com>
2022-10-17 03:30:22 +0300

    m

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,10 @@
+import FormDisabledInputExample from "./Form";
 const Header = () => {
   return (
     <div>
       <h1>Task Tracker</h1>{" "}
       <button className="bg-danger text-light">Show Add Task Bar</button>
+      <FormDisabledInputExample/>
     </div>
   );
 };
